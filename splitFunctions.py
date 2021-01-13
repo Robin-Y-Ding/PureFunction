a6d2c1e4f62a86e5a944db71c2a23b035edc855d
Robin-Y-Ding <42725563+Robin-Y-Ding@users.noreply.github.com>
2021-01-13 17:59:28 -0500

    Add functions to translate json to source files

--- splitFunctions.py
+++ splitFunctions.py
@@ -1,6 +1,8 @@
 import subprocess
 import json
 import sys
+import os
+
 
 def cmd2result(cmd):
 	result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
@@ -14,10 +16,11 @@ def splitFunctions(fileDir):
 	cmd = "find " + fileDir + " -name '*.c'"
 	result1 = cmd2result(cmd)
 
-	cmd = "find " + fileDir + " -name '*.cpp'"
-	result2 = cmd2result(cmd)
+	# cmd = "find " + fileDir + " -name '*.cpp'"
+	# result2 = cmd2result(cmd)
 
-	result = result1 + result2
+	# result = result1 + result2
+	result = result1
 
 	dictResult = list()
 	filecnt = 0
@@ -41,5 +44,28 @@ def splitFunctions(fileDir):
 		json.dump(dictResult, fi, indent=2)
 
 
+def readFunc(func):
+	with open(func["file"], 'r', encoding="ISO-8859-1") as f:
+		lines = f.readlines()
+		funcStr = ''.join(lines[(func["begin"][0]-1):func["end"][0]])
+	return funcStr
+
+
+def extractJson(jsonFile, tgtDir):
+	with open(jsonFile, 'r') as jf:
+		functions = json.load(jf)
+		for func in functions:
+			try:
+				code = readFunc(func)
+			except:
+				continue
+			fileBase = os.path.basename(func["file"])
+			tgtFileBase = func["function"] + "_" + fileBase
+			tgtFile = os.path.join(tgtDir, tgtFileBase)
+			with open(tgtFile, "w") as tf:
+				tf.write(code)
+
+
 if __name__ == '__main__':
-	splitFunctions(sys.argv[1])
+	# splitFunctions(sys.argv[1])
+	extractJson(sys.argv[1], sys.argv[2])
